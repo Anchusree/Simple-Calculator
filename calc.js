2436222099cddf31ead88f899afb13661bb855c9
Anchusree <anchusreen@gmail.com>
2020-08-09 10:49:47 +0300

    started with simple calculator project

---
+++ calc.js
@@ -0,0 +1,13 @@
+
+
+let display = document.getElementById("display");
+
+
+function btton(val){
+    document.getElementById("display").value += val
+    console.log(val)
+}
+function clearbtton(){
+    document.getElementById("display").value = " " 
+}
+
